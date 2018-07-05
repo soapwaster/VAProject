2842aa31b428573eff10657c2c5a41e8e214c38d
spallas <davide.spallaccini@gmail.com>
2018-07-05 13:22:26 +0200

    Add true data for delay views

--- data_preprocesing.py
+++ data_preprocesing.py
@@ -16,7 +16,6 @@ def save_pca(df):
     
     principalDf = pd.DataFrame(data = principalComponents, 
                                 columns = ['principal component 1', 'principal component 2'])
-    print(type(principalDf.values[:5]))
     
     principalDf["A"] = df["ARR_DELAY_NEW"]
     principalDf["D"] = df["DEP_DELAY_NEW"]
@@ -31,36 +30,52 @@ def save_pca(df):
             np.savetxt(f, line, delimiter="\t")
     
 
-data_frame = pd.read_csv("FDB/1k-flights.csv")
+months_names = ['January', 'February', 'March', 'April', 'May', 'June', 
+                'July', 'August', 'September','October','November', 'December']              
+frames_par = []
+frames_pca = []
 
-features = ["ARR_DELAY_NEW","DEP_DELAY_NEW", "AIR_TIME", "DISTANCE", 
-            "CARRIER_DELAY", "NAS_DELAY",
-            "LATE_AIRCRAFT_DELAY", "ORIGIN_STATE_ABR"]
+for m in months_names:
+    
+    data_frame = pd.read_csv("../2017-flights/" + m + ".csv")
+    
+    features = ["ARR_DELAY_NEW","DEP_DELAY_NEW", "AIR_TIME", "DISTANCE", 
+                "CARRIER_DELAY", "NAS_DELAY", "LATE_AIRCRAFT_DELAY", 
+                "ORIGIN_STATE_ABR", "CANCELLED"]
 
-df = data_frame.loc[:, features]
+    df = data_frame.loc[:, features]
+    df = df[ df["CANCELLED"] == 0 ]
+    df = df.loc[:, features[:-1]]
+    
+    df = df.fillna(0.0)
+    
+    # optional filter out delays
+    df = df[((df["ARR_DELAY_NEW"] != 0) | (df["DEP_DELAY_NEW"] != 0))]
 
-df = df.fillna(0.0)
+    # raw manual outliers filter
+    cap_c = df["CARRIER_DELAY"]<100
+    cap_n = df["NAS_DELAY"]<100
+    cap_l = df["LATE_AIRCRAFT_DELAY"]<200
 
-cap_c = df["CARRIER_DELAY"]<100
-# cap_w = df["WEATHER_DELAY"]<100
-cap_n = df["NAS_DELAY"]<100
-cap_l = df["LATE_AIRCRAFT_DELAY"]<200
+    df = df[(df["ARR_DELAY_NEW"] < 150)
+            & (df["DEP_DELAY_NEW"] < 150)
+            & (df["DISTANCE"] < 3000)
+            & (df["AIR_TIME"] > 0)
+            & cap_c & cap_n & cap_l]
+    
+    frames_par.append(df.sample(32))
+    frames_pca.append(df.sample(92))
+    
+    print(m + " - Done")
 
-df = df[(df["ARR_DELAY_NEW"] < 150)
-        & (df["DEP_DELAY_NEW"] < 150)
-        & (df["DISTANCE"] < 3000)
-        & (df["AIR_TIME"] > 0)
-        & cap_c & cap_n & cap_l]
-        
-if FILTER_DELAY:
-    df = df[(df["ARR_DELAY_NEW"] != 0) | (df["DEP_DELAY_NEW"] != 0)]
+df_pca = pd.concat(frames_pca)
+df_par = pd.concat(frames_par)
 
-df = df.reset_index()
-
-print(df.head())
+df_pca = df_pca.reset_index()
+df_par = df_par.reset_index()
 
 with open("FDB/1k-6num-attrs.csv", "w") as f:
-    df.to_csv(f, index=False, columns=features)
+    df_par.to_csv(f, index=False, columns=features[:-1])
     
-save_pca(df.loc[:, features[:-1]])
+# save_pca(df_pca.loc[:, features[:-2]])
 
